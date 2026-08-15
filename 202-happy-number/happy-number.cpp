d140d591697dba3290db032010a4e7db4db9ec01
devanshi2208 <devanshisharma0508@gmail.com>
2026-08-15 21:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.38%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sumSquare(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = sumSquare(slow);
+            fast = sumSquare(sumSquare(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+};
